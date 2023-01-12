57464f14d059f0c3ca05a0338c2cd676bc36a1f9
Sagarmm0 <118435908+Sagarmm0@users.noreply.github.com>
2023-01-12 17:17:38 +0530

    Create Sa.tf

---
+++ Sa.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_version = "~> 1.3.7" // added latest version
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+     }
+    }
+   }
+
+provider "aws" {
+        region = "us-east-1" 
+        #profile = "default"
+	}
+
+resource "aws_instance" "SAGARinfra" {
+	ami = "ami-0b5eea76982371e91" 
+	instance_type = "t2.small"
+  tags = {
+    "Name" = "Terraform1"
+  }
+}
